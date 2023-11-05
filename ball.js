b24156f021b57ac3286ff0804847f152aeb56866
Nouras Hanana <nourashanana@Nourass-Air.fritz.box>
2023-11-05 15:38:51 +0100

    restart button

--- ball.js
+++ ball.js
@@ -1,5 +1,6 @@
 class Ball {
   constructor(x, y, size, color) {
+    //position X, Y
     this.x = x;
     this.y = y;
     this.size = size;
@@ -14,7 +15,7 @@ class Ball {
   }
 
   move() {
-    this.y += 14; // Adjust the speed if necessary
+    this.y += 14;
   }
 
   isBelowLine(lineY) {
@@ -22,18 +23,21 @@ class Ball {
   }
 }
 
-// main.js
+// coordinates the timing of these actions and checks
+// the game logic, such as determining if balls have crossed the decisionLineY
+//or if the player has interacted with them correctly based on their color
 
 class Game {
   constructor(canvas, lossCounterElement) {
+    // reference to the HTML element on the page that will display the number of losses.
     this.canvas = canvas;
     this.ctx = canvas.getContext("2d");
     this.lossCounterElement = lossCounterElement;
-    this.balls = [];
-    this.losses = 0;
-    this.ballFrequency = 1600; // How often to create balls
+    this.balls = []; // A list to keep all the balls that are created.
+    this.losses = 0; // the counter for how many losses
+    this.ballFrequency = 1400; // How often to create balls
     this.ballSize = 30;
-    this.decisionLineY = canvas.height - 240;
+    this.decisionLineY = canvas.height - 360;
     this.loseColor = "#B11111"; // Color that should not be clicked and can pass
     this.colors = [
       "#DC40B0",
@@ -44,21 +48,24 @@ class Game {
       "#00FFFF",
     ];
 
-    // Binding the methods to the instance
+    // Bind the instance methods to ensure 'this' refers to the Game instance when they are used as callbacks.
     this.update = this.update.bind(this);
     this.createBall = this.createBall.bind(this);
     this.handleKeyPress = this.handleKeyPress.bind(this);
+    this.restartButton = document.getElementById("restart-button");
+    this.restartButton.addEventListener("click", () => this.resetGame());
 
     // Setting up the event listener for key presses
     document.addEventListener("keydown", this.handleKeyPress);
   }
 
+  //creating the randomness of the losecolor
   createBall() {
-    const isLoseColor = Math.random() < 2 / 7; // Chance to create a loseColor ball
+    const isLoseColor = Math.random() < 2 / 7; // 2 out of 7 Chance to create a loseColor ball
     const color = isLoseColor
       ? this.loseColor
       : this.colors[Math.floor(Math.random() * this.colors.length)];
-    const newBall = new Ball(this.canvas.width / 2, 0, this.ballSize, color);
+    const newBall = new Ball(this.canvas.width / 2, 0, this.ballSize, color); // centering the ball
     this.balls.push(newBall);
   }
 
@@ -80,11 +87,11 @@ class Game {
       if (ball.isBelowLine(this.decisionLineY)) {
         if (ball.color !== this.loseColor) {
           this.losses++;
-          this.lossCounterElement.textContent = "Losses: " + this.losses;
+          this.lossCounterElement.textContent = "Losses: " + this.losses; // Update the display with the current number of losses
           if (this.losses >= 4) {
             console.log("Game Over!");
             this.gameOver();
-            return; // Stop the game loop
+            return;
           }
         }
         this.balls.splice(i, 1); // Remove the ball after crossing the line
@@ -97,6 +104,7 @@ class Game {
   }
 
   handleKeyPress(event) {
+    // Execute when the space bar is pressed
     if (event.code === "Space") {
       for (let i = this.balls.length - 1; i >= 0; i--) {
         const ball = this.balls[i];
@@ -114,19 +122,29 @@ class Game {
   }
 
   startGameLoop() {
-    // Delay ball creation for 1 second
+    // Delay ball creation for 2 second
     setTimeout(() => {
       this.ballCreationInterval = setInterval(
-        this.createBall,
-        this.ballFrequency
+        this.createBall, //line 62
+        this.ballFrequency // 32 line/ 39
       );
     }, 2000);
 
     this.update(); // Start the game update loop using the bound function
   }
 
+  resetGame() {
+    this.balls = []; // Reset the balls array
+    this.losses = 0; // Reset the losses
+    this.lossCounterElement.textContent = "Losses: 0"; // Reset the loss counter display
+    this.restartButton.classList.add("hidden"); // Hide the restart button
+    clearInterval(this.ballCreationInterval); // Clear the interval for creating balls
+    this.startGameLoop(); // Restart the game loop
+  }
+
   gameOver() {
     clearInterval(this.ballCreationInterval);
+    this.restartButton.classList.remove("hidden");
     // Additional game over logic can be added here
   }
 }
